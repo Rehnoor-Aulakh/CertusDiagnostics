093fa3f11f598c853c6bbb1c422dd1349db39b1d
Rehnoor Aulakh <aulakhrehnoor@gmail.com>
2026-07-23 11:18:23 +0530

    Adjusted the position of mobile navbar- updated

--- Certus-Client/src/components/Logo.jsx
+++ Certus-Client/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 export default function Logo() {
   return (
     <div className="logo-container flex items-center gap-1 md:gap-4">
-      <img src="/logo10.png" alt="Certus Diagnostics" className="h-12 md:h-16 w-auto" />
+      <img src="/logo10.png" alt="Certus Diagnostics" className="h-14 md:h-16 w-auto" />
       <img
         src="/thyrocare.png"
         alt="Thyrocare"
